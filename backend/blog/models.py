04bc0b513d4c22427aa71af83bdfa1d75151bc11
Basharat Ahmad Mir <95496189+Basharat908@users.noreply.github.com>
2025-03-29 07:26:41 +0530

    Update models.py

--- backend/blog/models.py
+++ backend/blog/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 class BlogPost(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
-    file = models.FileField(upload_to='blog_files/', null=True, blank=True)
+    file = models.FileField(upload_to='jnv/', null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
