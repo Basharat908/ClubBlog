813f742965cc595da92fdeb64192c3eff8aec5e1
Basharat Ahmad Mir <95496189+Basharat908@users.noreply.github.com>
2025-03-29 07:37:55 +0530

    Update urls.py

--- backend/club_blog/urls.py
+++ backend/club_blog/urls.py
@@ -41,5 +41,5 @@ urlpatterns = [
 ]
 
 # Only serve media files this way in development
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
